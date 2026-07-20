f9100a7b8240945c23e7d580fab1994ba75469ce
zhaogongchengsi <zzh1586169624@163.com>
2026-07-20 14:08:30 +0800

    Save uncommitted changes

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,6 @@
 export { ApiError, http, HttpClient, setApiKey } from './http/index.js'
 export type { ApiResponse, RequestConfig } from './http/index.js'
 
-export {
-  dramaRecap,
-  dramaRecapAndWait,
-  dramaRecapVertical,
-  dramaRecapVerticalAndWait,
-  dramaScript,
-  dramaScriptAndWait,
-  enhanceVideo,
-  enhanceVideoAndWait,
-  eraseVideoSubtitlePro,
-  eraseVideoSubtitleProAndWait,
-  eraseVideoSubtitleStandard,
-  eraseVideoSubtitleStandardAndWait,
-  getTask,
-  Mediakit,
-  videoUnderstand,
-  videoUnderstandAndWait,
-  waitForTask,
-} from './mediakit/index.js'
-export type {
-  CreateTaskResponse,
-  MediakitConfig,
-  TaskResult,
-  TaskStatus,
-  WaitForTaskOptions,
-} from './mediakit/index.js'
-export type {
-  DramaRecapParams,
-  DramaRecapResult,
-} from './mediakit/index.js'
-export type {
-  DramaRecapVerticalParams,
-  DramaRecapVerticalResult,
-} from './mediakit/index.js'
-export type {
-  DramaScriptParams,
-  DramaScriptResult,
-} from './mediakit/index.js'
-export type {
-  EnhanceVideoParams,
-  EnhanceVideoResult,
-} from './mediakit/index.js'
-export type {
-  EraseVideoSubtitleParams,
-  EraseVideoSubtitleResult,
-} from './mediakit/index.js'
-export type {
-  VideoUnderstandManualOption,
-  VideoUnderstandParams,
-  VideoUnderstandResult,
-  VideoUnderstandTokenUsage,
-} from './mediakit/index.js'
+/* ── 从 mediakit 重新导出所有内容 ── */
+export * from './mediakit/index.js'
+export type * from './mediakit/index.js'
